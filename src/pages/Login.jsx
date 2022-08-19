570a07aab7d92b71295b624da9459ed19a913393
Kleiton Ewerton <kleitonewertonoliveira@gmail.com>
2022-08-19 11:44:14 -0300

    best login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
+import {BsFillArrowLeftSquareFill} from 'react-icons/bs';
 //------------------------- CSS -------------------------//
 
 import "../styles.css";
@@ -11,6 +12,7 @@ function Login() {
     <div className="login-header">
       <div className="left-header">Form</div>
       <div className="right-header"></div>
+      <Link to="/home" style={{position: 'absolute',zIndex:'2', margin:'10px' }}> <BsFillArrowLeftSquareFill style={{fontSize:'30px', color:"#d1c1e0"}}/></Link>
     </div>
   );
 }
