916be994c2527e87efc0c951e67e611b26153287
Kleiton Ewerton <kleitonewertonoliveira@gmail.com>
2022-08-28 17:58:44 -0300

    table members add

--- src/pages/adm/DashBoardAdm.jsx
+++ src/pages/adm/DashBoardAdm.jsx
@@ -1,15 +1,50 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import SideBar from "./SideBar";
+// import { IconName } from "react-icons/bs";
+import { AiOutlineUserAdd,AiFillEdit,AiFillDelete,AiFillEye } from "react-icons/ai";
 
-class DashBoardAdm extends Component {
-  state = {};
-  render() {
-    return (
-      <div>
-        <div>1</div>
-        <div>2</div>
+import './DashBoardAdm.css';
+import "../../styles.css";
+
+
+function DashBoardAdm() {
+  const [members, setMembers] = useState([]);
+
+  const getMembers = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMembers(data);
+  };
+
+  useEffect(() => {
+    const urlMembers = "http://localhost:5000/members";
+    getMembers(urlMembers);
+  }, []);
+
+  return (
+    <div>
+      <SideBar></SideBar>
+      <div className="dashBoard">
+        <table className="membersTable">
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Cargo</th>
+            <th>Util</th>
+          </tr>
+          {members.map((member, index) => (
+            <tr key={member.id}>
+              <td>{member.name}</td>
+              <td> {member.email}</td>
+              <td>{member.cargo}</td>
+              <td><AiFillEdit></AiFillEdit><AiFillDelete></AiFillDelete><AiFillEye></AiFillEye></td>
+            </tr>
+          ))}
+        </table>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default DashBoardAdm;
